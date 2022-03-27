e497e28576e5f60145f3927bfb3c6f9dc9950722
Maverick-2000 <stevebobygeorge@gmail.com>
2022-03-27 19:06:55 +0530

    Refactor Private Routes

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -5,5 +5,5 @@ import { useAuth } from "../../contexts/AuthContext";
 export default function PrivateRoute({ children }) {
   const { currentUser } = useAuth();
 
-  return currentUser ? children : <Navigate to="/" />;
+  return currentUser ? children : <Navigate to="/login" />;
 }
