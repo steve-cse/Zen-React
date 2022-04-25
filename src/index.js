942078740de0e484250699c88d4eacf490285289
Maverick-2000 <stevebobygeorge@gmail.com>
2022-04-25 07:22:10 +0530

    Support for PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -12,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
