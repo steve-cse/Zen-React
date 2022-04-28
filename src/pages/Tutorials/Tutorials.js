5c0ddf1ad0112d1e1e945b733ed047fd202ce1ee
Maverick-2000 <stevebobygeorge@gmail.com>
2022-04-28 23:50:03 +0530

    Add react player

--- src/pages/Tutorials/Tutorials.js
+++ src/pages/Tutorials/Tutorials.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SecNavBar from "../../components/SecNavBar/SecNavBar";
 import { MinimalFooter } from "../../containers";
+import ReactPlayer from "react-player/lazy";
 import "./Tutorials.css";
 export default function Tutorials() {
   return (
@@ -8,248 +9,173 @@ export default function Tutorials() {
       <SecNavBar />
       <h2 className="tutorials_heading">Tutorials</h2>
       <div className="tutorials_container">
-        <iframe
-          className="tutorials_iframe_1"
-          loading="lazy"
-          title="Intro"
-          src="https://www.youtube.com/embed/IxtOtPFwaIE?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/IxtOtPFwaIE?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_2"
-          loading="lazy"
-          title="Chair Yoga Pose"
-          src="https://www.youtube.com/embed/3cmRrQAZqRU?rel=0&modestbranding=1"
-         
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/3cmRrQAZqRU?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_3"
-          loading="lazy"
-          title="Cobra Yoga Pose"
-          src="https://www.youtube.com/embed/gWpUTLghv_0?rel=0&modestbranding=1"
-         
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/gWpUTLghv_0?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_4"
-          loading="lazy"
-          title="Goddess Yoga Pose"
-          src="https://www.youtube.com/embed/wgXefCprLm0?rel=0&modestbranding=1"
-        
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/wgXefCprLm0?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_5"
-          loading="lazy"
-          title="Tree Yoga Pose"
-          src="https://www.youtube.com/embed/yVE4XXFFO70?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/yVE4XXFFO70?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_6"
-          loading="lazy"
-          title="Triangle Yoga Pose"
-          src="https://www.youtube.com/embed/_oixJ56znyU?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/_oixJ56znyU?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_7"
-          loading="lazy"
-          title="Camel Yoga Pose"
-          src="https://www.youtube.com/embed/_NNnowkcIqU?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/_NNnowkcIqU?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_8"
-          loading="lazy"
-          title="Plank Yoga Pose"
-          src="https://www.youtube.com/embed/RG4PwUP5njo?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/RG4PwUP5njo?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_9"
-          loading="lazy"
-          title="Upward Dog Yoga Pose"
-          src="https://www.youtube.com/embed/pVmOOluGAv8?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/pVmOOluGAv8?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_10"
-          loading="lazy"
-          title="Warrior 1 Yoga Pose"
-          src="https://www.youtube.com/embed/4Tbp3NydcbQ?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/4Tbp3NydcbQ?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_11"
-          loading="lazy"
-          title="Warrior 2 Yoga Pose"
-          src="https://www.youtube.com/embed/7Xl-TNyOJec?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/7Xl-TNyOJec?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_12"
-          loading="lazy"
-          title="Bound Ankle Yoga Pose"
-          src="https://www.youtube.com/embed/B6tb4TncKhY?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/B6tb4TncKhY?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_13"
-          loading="lazy"
-          title="Bridge Yoga Pose"
-          src="https://www.youtube.com/embed/NnbvPeAIhmA?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/NnbvPeAIhmA?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_14"
-          loading="lazy"
-          title="Down Dog Yoga Pose"
-          src="https://www.youtube.com/embed/EC7RGJ975iM?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/EC7RGJ975iM?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_15"
-          loading="lazy"
-          title="Gate Yoga Pose"
-          src="https://www.youtube.com/embed/_xKgCrr_n9g?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/_xKgCrr_n9g?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_16"
-          loading="lazy"
-          title="Half Moon Yoga Pose"
-          src="https://www.youtube.com/embed/csErxVR7dpA?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/csErxVR7dpA?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_17"
-          loading="lazy"
-          title="Right Bicep Curl"
-          src="https://www.youtube.com/embed/ykJmrZ5v0Oo?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/ykJmrZ5v0Oo?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_18"
-          loading="lazy"
-          title="Left Bicep Curl"
-          src="https://www.youtube.com/embed/J_OQDNM8gn8?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/J_OQDNM8gn8?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_19"
-          loading="lazy"
-          title="Lateral Raise"
-          src="https://www.youtube.com/embed/YFRh8TWjPZ0?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/YFRh8TWjPZ0?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_20"
-          loading="lazy"
-          title="Lunges"
-          src="https://www.youtube.com/embed/3XDriUn0udo?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/3XDriUn0udo?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_21"
-          loading="lazy"
-          title="Side Lunge"
-          src="https://www.youtube.com/embed/tlUg1DXhHm8?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/tlUg1DXhHm8?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_22"
-          loading="lazy"
-          title="Squats"
-          src="https://www.youtube.com/embed/YaXPRqUwItQ?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/YaXPRqUwItQ?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_23"
-          loading="lazy"
-          title="Tricep Kickback"
-          src="https://www.youtube.com/embed/m9me06UBPKc?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/m9me06UBPKc?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
-        <iframe
-          className="tutorials_iframe_24"
-          loading="lazy"
-          title="Plie Squat"
-          src="https://www.youtube.com/embed/QWOY17an9yQ?rel=0&modestbranding=1"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
-          allowFullScreen
-          showinfo="1"
+        <ReactPlayer
+          url="https://www.youtube.com/embed/QWOY17an9yQ?rel=0&modestbranding=1"
+          width="300px"
+          height="150px"
+          controls
+          light
         />
       </div>
       <MinimalFooter />
