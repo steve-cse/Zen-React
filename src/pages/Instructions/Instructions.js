b28cb1f9f265ea8deae652a3f54cdf3ee1e0879d
Maverick-2000 <stevebobygeorge@gmail.com>
2022-04-29 23:30:49 +0530

    Switch to @react-pdf-viewer

--- src/pages/Instructions/Instructions.js
+++ src/pages/Instructions/Instructions.js
@@ -1,25 +1,25 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import SecNavBar from "../../components/SecNavBar/SecNavBar";
 import { MinimalFooter } from "../../containers";
-import CloudPdfViewer from "@openbook/cloudpdf-viewer";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import manual from "../../assets/manual.pdf"
 import "./Instructions.css";
 export default function Instructions() {
-  const viewer = useRef(null);
-  useEffect(() => {
-    CloudPdfViewer(
-      {
-        documentId: "73bf6f9a-e988-426a-8fea-6601b81cb299",
-        darkMode: false,
-      },
-      viewer.current
-    );
-  }, []);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className="Instructions">
       <SecNavBar />
       <h2 className="instructions_heading">User Guide</h2>
       <div className="instructions_container">
-        <div className="viewer" ref={viewer}></div>
+        <div className="viewer_container">
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.min.js">
+            <Viewer plugins={[defaultLayoutPluginInstance]} fileUrl={manual}></Viewer>
+          </Worker>
+        </div>
       </div>
       <MinimalFooter />
     </div>
